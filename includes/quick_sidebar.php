5234a12f6c51b90c971d51eb4b24263c82e75e99
Akkadius <akkadius1@gmail.com>
2015-02-21 19:39:01 -0600

    More code structure adjustments

--- includes/quick_sidebar.php
+++ includes/quick_sidebar.php
@@ -1,40 +1,41 @@
 <?php
-	echo '<!-- BEGIN QUICK SIDEBAR -->
-	<a href="javascript:;" class="page-quick-sidebar-toggler"><i class="icon-close"></i></a>
-	<div class="page-quick-sidebar-wrapper ">
-		<div class="page-quick-sidebar">
-			<div class="nav-justified">
-				<ul class="nav nav-tabs nav-justified">
-					<li class="active">
-						<a href="#quick_sidebar_tab_1" data-toggle="tab">
-                            Actions
-					    </a>
-					</li>
+	echo '
+	<!-- BEGIN QUICK SIDEBAR -->
+        <a href="javascript:;" class="page-quick-sidebar-toggler"><i class="icon-close"></i></a>
+        <div class="page-quick-sidebar-wrapper ">
+            <div class="page-quick-sidebar">
+                <div class="nav-justified">
+                    <ul class="nav nav-tabs nav-justified">
+                        <li class="active">
+                            <a href="#quick_sidebar_tab_1" data-toggle="tab">
+                                Actions
+                            </a>
+                        </li>
 
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						    More<i class="fa fa-angle-down"></i>
-						</a>
-						<ul class="dropdown-menu pull-right" role="menu" id="quick_sidebar_menu_options">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                More<i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu pull-right" role="menu" id="quick_sidebar_menu_options">
 
-						</ul>
-					</li>
-				</ul>
-				<div  class="tab-content slimScrollDiv">
-                    <div class="tab-pane active page-quick-sidebar-chat" id="quick_sidebar_tab_1">
-                        <div class="page-quick-sidebar-chat-users" data-rail-color="#ddd" data-wrapper-class="page-quick-sidebar-list">
-                            <div id="quick_sidebar_content"></div>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div  class="tab-content slimScrollDiv">
+                        <div class="tab-pane active page-quick-sidebar-chat" id="quick_sidebar_tab_1">
+                            <div class="page-quick-sidebar-chat-users" data-rail-color="#ddd" data-wrapper-class="page-quick-sidebar-list">
+                                <div id="quick_sidebar_content"></div>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                            </div>
                         </div>
                     </div>
                 </div>
-			</div>
-		</div>
-	</div>
-	<!-- END QUICK SIDEBAR -->
-	</div>';
+            </div>
+        </div>
+        <!-- END QUICK SIDEBAR -->
+        </div>';
 ?>
