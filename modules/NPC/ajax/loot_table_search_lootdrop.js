76d342e39f0c4190cbfc8e1fba34c42a986e2ac5
Akkadius <akkadius1@gmail.com>
2015-03-05 04:25:34 -0600

    [NPC] Adding lootdrop entries into the loot table are now functional

--- modules/NPC/ajax/loot_table_search_lootdrop.js
+++ modules/NPC/ajax/loot_table_search_lootdrop.js
@@ -1,4 +1,48 @@
 /**
  * Created by
- * Akakdius on 3/5/2015.
+ * Akkadius on 3/5/2015.
  */
+
+$(document).ready(function() {
+    var loot_table = $(".loot_table_sub").DataTable( {
+        scrollY:        "205px",
+        sScrollXInner: "400px",
+        scrollCollapse: true,
+        paging:         false,
+        "searching": false,
+        "ordering": true,
+        "bSort" : false,
+    } );
+
+    var timer = setInterval(function () {
+        loot_table.draw();
+        window.clearInterval(timer);
+    }, 500);
+});
+
+function do_loot_search(search_val, loot_table){
+    $.ajax({
+        url: "ajax.php?M=NPC&do_loot_search=" + search_val + "&loot_table=" + loot_table,
+        context: document.body
+    }).done(function(e) {
+        $("#loot_search_result").html(e);
+    });
+}
+
+function do_loot_table_loot_drop_add(loot_table, loot_drop){
+    $.ajax({
+        url: "ajax.php?M=NPC&do_loot_table_loot_drop_add=" + loot_table + "&loot_drop=" + loot_drop,
+        context: document.body
+    }).done(function(e) {
+        // if(e){ alert(e); }
+
+        $.ajax({
+            url: "ajax.php?M=NPC&load_npc_top_pane_dash=" + $('#top_right_pane').attr('npc_loaded'),
+            context: document.body
+        }).done(function(e) {
+            /* Update Data Table as well */
+            $('#top_right_pane').html(e).fadeIn();
+        });
+
+    });
+}
--- modules/NPC/ajax/loot_table_search_lootdrop.php
+++ modules/NPC/ajax/loot_table_search_lootdrop.php
@@ -10,7 +10,7 @@
     if($_GET['loottable_add']){
         $loot_table = $_GET['loottable_add'];
         $Content .= FormStart();
-        $Content .= FormInput('Lootdrop Search', '<input type="text" class="form-control" id="loot_search" onkeyup="do_loot_search(this.value)">');
+        $Content .= FormInput('Lootdrop Search', '<input type="text" class="form-control" id="loot_search" onkeyup="if(event.keyCode == 13){ do_loot_search(this.value, ' . $loot_table . ') }">');
         $Content .= FormInput('',
             '<a href="javascript:;" class="btn green btn-xs" onclick="do_loot_search($(\'#loot_search\').val())">
                     <i class="fa fa-search"></i>
@@ -21,6 +21,59 @@
         $Content .= '<div id="loot_search_result"></div>';
         echo Modal('Loot Table, Search for Lootdrop', $Content, '');
         echo '<script type="text/javascript" src="modules/NPC/ajax/loot_table_search_lootdrop.js"></script>';
+    }
+    /* Loot Table :: Loot Drop Table Search */
+    if($_GET['do_loot_search']){
+        $search_string = $_GET['do_loot_search'];
+        $loot_table = $_GET['loot_table'];
+
+        echo '<table class="table table-condensed table-hover table-bordered loot_table_sub">
+                    <thead>
+                        <tr>
+                            <th style="width:50px"></th>
+                            <th style="width:25px">ID</th>
+                            <th style="width:20px;text-align:center"><small>Item<br>Count</small></th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                ';
+        $result = mysql_query(
+            "SELECT
+            lootdrop.id,
+            lootdrop.`name`,
+            COUNT(lootdrop_entries.item_id) as item_count
+            FROM
+            lootdrop
+            INNER JOIN lootdrop_entries ON lootdrop.id = lootdrop_entries.lootdrop_id
+            WHERE `name` LIKE '%" . $search_string . "%'
+            GROUP BY lootdrop.id
+            ORDER BY item_count DESC
+        ");
+
+        while($row = mysql_fetch_array($result)) {
+            echo '
+                <tr>
+                    <td style="text-align:center">
+                        <a href="javascript:;" class="btn green btn-xs" onclick="do_loot_table_loot_drop_add(' . $loot_table . ', ' . $row['id'] . ')">
+                            <i class="fa fa-plus"></i>
+                            Add
+                        </a>
+                    </td>
+                    <td style="text-align:center">' . $row['id'] . '</td>
+                    <td style="text-align:center">' . $row['item_count'] . '</td>
+                    <td>' . $row['name'] . '</td>
+                </tr>';
+        }
+        echo '</table>';
+        echo '<script type="text/javascript" src="modules/NPC/ajax/loot_table_search_lootdrop.js"></script>';
+    }
+    /* Loot Table :: DB Add Loot Drop to Loot Table */
+    if($_GET['do_loot_table_loot_drop_add']) {
+        $loot_table = $_GET['do_loot_table_loot_drop_add'];
+        $loot_drop = $_GET['loot_drop'];
+        $result = mysql_query('INSERT INTO `loottable_entries` (loottable_id, lootdrop_id, multiplier, probability, droplimit, mindrop)
+            VALUES (' . $loot_table . ', ' . $loot_drop . ', 1, 100, 1, 1)');
+        if(!$result){ echo mysql_error(); }
     }
 
 ?>
