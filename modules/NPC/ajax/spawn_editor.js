f075e50759207909073258aa6c630392c8e35149
Akkadius <akkadius1@gmail.com>
2015-03-08 12:59:47 -0500

    [NPC] Spawn Editor, tables now editable

--- modules/NPC/ajax/spawn_editor.js
+++ modules/NPC/ajax/spawn_editor.js
@@ -2,3 +2,73 @@
  * Created by Akkadius on 3/8/2015.
  */
 
+$(document).ready(function() {
+    /* Hook Mouse Enter and Leave Events */
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).unbind("mouseenter");
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).bind("mouseenter", function() {
+
+        width = $(this).css("width");
+        height = $(this).css("height");
+        data = $(this).html();
+
+        /* If attr is set to non edit, return */
+        if($(this).attr("nonedit") == 1){
+            return;
+        }
+
+        /* Dont replace the button */
+        if(data.match(/button/i)){ return; }
+
+        $(this).html('<input type="text" class="form-control" value="' + data + '" onchange="">');
+        $(this).children("input").css('width', (parseInt(width) * 1));
+        $(this).children("input").css('height', (parseInt(height)));
+        $(this).children("input").css("font-size", "11px");
+        // $('textarea').autosize();
+        data = "";
+    });
+
+    /* Hook Mouse Enter and Leave Events */
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).unbind("mouseleave");
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).bind("mouseleave", function() {
+        data = "";
+
+        /* If attr is set to non edit, return */
+        if($(this).attr("nonedit") == 1){
+            return;
+        }
+
+        /* Grab data from cell depending on input type */
+        if($(this).has("select").length){
+            data = $(this).children("select").val();
+        }
+        else if($(this).has("input").length){
+            data = $(this).children("input").val();
+        }
+
+        /* If cell contains cell... skip */
+        if($(this).has("button").length){ return; }
+
+        $(this).html(data);
+        data = "";
+    });
+
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).unbind("change");
+    $( ".spawn_entry td, .spawn2 td, .spawn_group td" ).bind("change", function() {
+        field_name = $(this).attr("field_name");
+        db_table = $(this).parent('tr').attr("db_table");
+        db_key = $(this).parent('tr').attr("db_key");
+        db_key_val = $(this).parent('tr').attr("db_key_val");
+        data = $(this).children('input').val();
+
+        console.log("html " + $(this).html());
+        console.log("val " + data);
+
+        /* Do table update */
+        $.ajax({
+            url: "ajax.php?M=NPC&do_spawn_edit_update=" + db_table + "&field=" + field_name + "&value=" + data + "&db_key=" + encodeURIComponent(db_key) + "&db_key_val=" + encodeURIComponent(db_key_val),
+            context: document.body
+        }).done(function(e) {
+            Notific8("NPC Editor", "Updated " + field_name + " to value " + data, 2000);
+        });
+    });
+});
--- modules/NPC/ajax/spawn_editor.php
+++ modules/NPC/ajax/spawn_editor.php
@@ -14,7 +14,7 @@
             .spawn_table table tbody th{ font-size:12px !important; }
             .spawn_table table tbody tr{ height:10px !important; }
             .spawn_table td{ padding: 1px !important; text-align:center !important;  }
-            .spawn_table table tbody td input{ text-align:center; height:20px !important; }
+            .spawn_table input{ text-align:center; height:20px !important; width:100% !important;  }
             .spawn_table th { padding:1px !important; font-size: 12px !important; text-align:center }
         </style>';
 
@@ -27,46 +27,44 @@
 
         $spawn_group_id = $spawn_entry['spawngroupID'];
 
-
-
         /* Spawn Group */
         $Content .= '<h3>Spawn Group</h3>';
         $Content .= '
-                <table class="table table-condensed table-hover table-bordered spawn_table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Spawn Limit</th>
-                            <th>Dist</th>
-                            <th>Min X</th>
-                            <th>Max X</th>
-                            <th>Mix Y</th>
-                            <th>Max Y</th>
-                            <th>Delay</th>
-                            <th>Min Delay</th>
-                            <th>Despawn</th>
-                            <th>Despawn Timer</th>
-                        </tr>
-                    </thead>
+            <table class="table table-condensed table-hover table-bordered spawn_table spawn_group">
+                <thead>
+                    <tr>
+                        <th style="width:100px">ID</th>
+                        <th style="width:100px">Name</th>
+                        <th style="width:100px">Spawn Limit</th>
+                        <th style="width:100px">Dist</th>
+                        <th style="width:100px">Min X</th>
+                        <th style="width:100px">Max X</th>
+                        <th style="width:100px">Mix Y</th>
+                        <th style="width:100px">Max Y</th>
+                        <th style="width:100px">Delay</th>
+                        <th style="width:100px">Min Delay</th>
+                        <th>Despawn</th>
+                        <th>Despawn Timer</th>
+                    </tr>
+                </thead>
         ';
 
         $result = mysql_query("SELECT * FROM `spawngroup` WHERE `id` = " . $spawn_group_id);
         while ($row = mysql_fetch_array($result)) {
             $Content .= '
-                <tr>
-                    <td style="background-color:yellow">' . $row['id'] . '</td>
-                    <td>' . $row['name'] . '</td>
-                    <td>' . $row['spawn_limit'] . '</td>
-                    <td>' . $row['dist'] . '</td>
-                    <td>' . $row['min_x'] . '</td>
-                    <td>' . $row['max_x'] . '</td>
-                    <td>' . $row['min_y'] . '</td>
-                    <td>' . $row['max_y'] . '</td>
-                    <td>' . $row['delay'] . '</td>
-                    <td>' . $row['mindelay'] . '</td>
-                    <td>' . $row['despawn'] . '</td>
-                    <td>' . $row['despawn_timer'] . '</td>
+                <tr db_table="spawngroup" db_key="id" db_key_val="' . $row['id'] . '">
+                    <td style="background-color:yellow" nonedit="1">' . $row['id'] . '</td>
+                    <td field_name="name">' . $row['name'] . '</td>
+                    <td field_name="spawn_limit">' . $row['spawn_limit'] . '</td>
+                    <td field_name="dist">' . $row['dist'] . '</td>
+                    <td field_name="min_x">' . $row['min_x'] . '</td>
+                    <td field_name="max_x">' . $row['max_x'] . '</td>
+                    <td field_name="min_y">' . $row['min_y'] . '</td>
+                    <td field_name="max_y">' . $row['max_y'] . '</td>
+                    <td field_name="delay">' . $row['delay'] . '</td>
+                    <td field_name="mindelay">' . $row['mindelay'] . '</td>
+                    <td field_name="despawn">' . $row['despawn'] . '</td>
+                    <td field_name="despawn_timer">' . $row['despawn_timer'] . '</td>
                 </tr>';
         }
         $Content .= '</table>';
@@ -74,47 +72,47 @@
         /* Spawn2 */
         $Content .= '<h3>Spawn2</h3>';
         $Content .= '
-                <table class="table table-condensed table-hover table-bordered spawn_table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Spawn Group ID</th>
-                            <th>Zone</th>
-                            <th>Version</th>
-                            <th>X</th>
-                            <th>Y</th>
-                            <th>Z</th>
-                            <th>Heading</th>
-                            <th>Respawn Time</th>
-                            <th>Variance</th>
-                            <th>Path Grid</th>
-                            <th>Condition</th>
-                            <th>Condition Value</th>
-                            <th>Enabled</th>
-                            <th>Animation</th>
-                        </tr>
-                    </thead>
+            <table class="table table-condensed table-hover table-bordered spawn_table spawn2">
+                <thead>
+                    <tr>
+                        <th style="width:100px">ID</th>
+                        <th style="width:100px">Spawn Group ID</th>
+                        <th style="width:150px">Zone</th>
+                        <th style="width:100px">Version</th>
+                        <th style="width:130px">X</th>
+                        <th style="width:130px">Y</th>
+                        <th style="width:130px">Z</th>
+                        <th style="width:170px">Heading</th>
+                        <th style="width:100px">Respawn Time</th>
+                        <th style="width:100px">Variance</th>
+                        <th style="width:100px">Path Grid</th>
+                        <th style="width:100px">Condition</th>
+                        <th style="width:100px">Condition Value</th>
+                        <th>Enabled</th>
+                        <th>Animation</th>
+                    </tr>
+                </thead>
         ';
 
         $result = mysql_query("SELECT * FROM `spawn2` WHERE `spawngroupID` = " . $spawn_group_id);
         while ($row = mysql_fetch_array($result)) {
             $Content .= '
-                <tr>
-                    <td>' . $row['id'] . '</td>
-                    <td style="background-color:yellow">' . $row['spawngroupID'] . '</td>
-                    <td>' . $row['zone'] . '</td>
-                    <td>' . $row['version'] . '</td>
-                    <td>' . $row['x'] . '</td>
-                    <td>' . $row['y'] . '</td>
-                    <td>' . $row['z'] . '</td>
-                    <td>' . $row['heading'] . '</td>
-                    <td>' . $row['respawntime'] . '</td>
-                    <td>' . $row['variance'] . '</td>
-                    <td>' . $row['pathgrid'] . '</td>
-                    <td>' . $row['_condition'] . '</td>
-                    <td>' . $row['cond_value'] . '</td>
-                    <td>' . $row['enabled'] . '</td>
-                    <td>' . $row['animation'] . '</td>
+                <tr db_table="spawn2" db_key="id,spawngroupID" db_key_val="' . $row['id'] . ',' . $row['spawngroupID'] . '">
+                    <td nonedit="1">' . $row['id'] . '</td>
+                    <td style="background-color:yellow" nonedit="1">' . $row['spawngroupID'] . '</td>
+                    <td field_name="zone">' . $row['zone'] . '</td>
+                    <td field_name="version">' . $row['version'] . '</td>
+                    <td field_name="x">' . $row['x'] . '</td>
+                    <td field_name="y">' . $row['y'] . '</td>
+                    <td field_name="z">' . $row['z'] . '</td>
+                    <td field_name="heading">' . $row['heading'] . '</td>
+                    <td field_name="respawntime">' . $row['respawntime'] . '</td>
+                    <td field_name="variance">' . $row['variance'] . '</td>
+                    <td field_name="pathgrid">' . $row['pathgrid'] . '</td>
+                    <td field_name="_condition">' . $row['_condition'] . '</td>
+                    <td field_name="cond_value">' . $row['cond_value'] . '</td>
+                    <td field_name="enabled">' . $row['enabled'] . '</td>
+                    <td field_name="animation">' . $row['animation'] . '</td>
                 </tr>';
         }
         $Content .= '</table>';
@@ -122,11 +120,11 @@
         /* Spawn Entry */
         $Content .= '<h3>Spawn Entry</h3>';
         $Content .= '
-                <table class="table table-condensed table-hover table-bordered spawn_table">
+                <table class="table table-condensed table-hover table-bordered spawn_table spawn_entry">
                     <thead>
                         <tr>
-                            <th>NPC ID</th>
-                            <th>Spawn Group ID</th>
+                            <th style="width:150px">NPC ID</th>
+                            <th style="width:150px">Spawn Group ID</th>
                             <th>Chance</th>
                         </tr>
                     </thead>
@@ -135,16 +133,38 @@
         $result = mysql_query("SELECT * FROM `spawnentry` WHERE `spawngroupID` = " . $spawn_group_id);
         while ($row = mysql_fetch_array($result)) {
             $Content .= '
-                <tr>
-                    <td style="background-color:orange">' . $row['npcID'] . '</td>
-                    <td style="background-color:yellow">' . $row['spawngroupID'] . '</td>
-                    <td>' . $row['chance'] . '</td>
+                <tr db_table="spawnentry" db_key="spawngroupID,npcID" db_key_val="' . $row['spawngroupID'] . ',' . $row['npcID'] . '">
+                    <td style="background-color:orange" nonedit="1">' . $row['npcID'] . '</td>
+                    <td style="background-color:yellow" nonedit="1">' . $row['spawngroupID'] . '</td>
+                    <td field_name="chance">' . $row['chance'] . '</td>
                 </tr>';
         }
         $Content .= '</table>';
 
         $Content .= '<script type="text/javascript" src="modules/NPC/ajax/spawn_editor.js"></script>';
         echo Modal('Spawn Editor', $Content, '');
+    }
+    /* Handle field updates from spawn editor tables */
+    if(isset($_GET['do_spawn_edit_update'])){
+        $table_name = $_GET['do_spawn_edit_update'];
+        $field = $_GET['field'];
+        $value = $_GET['value'];
+        $db_key = $_GET['db_key'];
+        $db_key_val = $_GET['db_key_val'];
+
+
+        if(preg_match('/,/i', $_GET['db_key'])){
+            $db_key = explode(",", $db_key);
+            $db_key_val = explode(",", $db_key_val);
+            $filter = $db_key[0] . " = " . $db_key_val[0] . " AND " . $db_key[1] . " = " . $db_key_val[1];
+        }
+        else{
+            $filter = $db_key . " = " . $db_key_val;
+        }
+
+        mysql_query("UPDATE `" . $table_name . "` SET `" . $field . "` = " . $value . " WHERE " . $filter);
+        echo "UPDATE `" . $table_name . "` SET `" . $field . "` = " . $value . " WHERE " . $filter;
+        echo mysql_error();
     }
 
 ?>
